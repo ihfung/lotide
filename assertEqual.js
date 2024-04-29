36b5ba5609264d6d8ea0c64c73b94527f690817a
ihleung <ihfungleung@gmail.com>
2024-04-29 19:38:37 -0400

    updated

---
+++ assertEqual.js
@@ -0,0 +1,30 @@
+/*// FUNCTION IMPLEMENTATION #1
+const sum = function (a, b) {
+  return a + b;
+};
+
+// TEST CODE
+console.assert(sum(1, 2) === 3);
+console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
+
+// FUNCTION IMPLEMENTATION #2
+const sumBuggy = function (a, b) {
+  return a * b;
+};
+
+// TEST CODE
+console.assert(sumBuggy(1, 2) === 3); // fails, because bug!
+*/
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log("Assertion failed: " + actual + " !== " + expected);
+  } else {
+    console.log("Assertion passed: " + actual + " === " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
