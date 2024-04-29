5d4c0e8c968acf72aa2d5d83386c12b495575e62
ihleung <ihfungleung@gmail.com>
2024-04-29 19:51:50 -0400

    updated

--- assertEqual.js
+++ assertEqual.js
@@ -17,11 +17,11 @@ console.assert(sumBuggy(1, 2) === 3); // fails, because bug!
 */
 
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual !== expected) {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected}`);
   } else {
-    console.log("✅✅✅ Assertion passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
   }
 };
 
