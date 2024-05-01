7560caba356183e1e938ee6e1858eaa416c53fe2
ihleung <ihfungleung@gmail.com>
2024-05-01 18:04:46 -0400

    updated

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  if (arr1.length !== arr2.length) {
+    result = false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let equal = eqArrays(actual, expected);
+  if (equal === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("letter").t, [2, 3]);
+assertArraysEqual(letterPositions("jason").s, [2]);
+assertArraysEqual(letterPositions("vanessa").s, [4, 5]);
