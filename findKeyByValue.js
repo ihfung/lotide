ac73fae6f1c1f374c30fee18dc208c8d3a83bd9a
ihleung <ihfungleung@gmail.com>
2024-05-01 18:23:01 -0400

    updated

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let findKey = Object.keys(object);
+  for (let i of findKey) {
+    if (object[i] === value) {
+      return i;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
