9d7f80d7c68a47c3334573e05cead1f0efb75810
ihleung <ihfungleung@gmail.com>
2024-04-30 20:06:19 -0400

    updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,8 @@
 const eqArrays = function (arr1, arr2) {
   let result = true;
+  if (arr1.length !== arr2.length) {
+    result = false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       result = false;
--- eqArrays.js
+++ eqArrays.js
@@ -8,6 +8,9 @@ const assertEqual = function (actual, expected) {
 
 const eqArrays = function (arr1, arr2) {
   let result = true;
+  if (arr1.length !== arr2.length) {
+    result = false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       result = false;
