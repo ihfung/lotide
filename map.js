0f4ba8f666462b3a35ad53f2ead87fecc82dfda4
ihleung <ihfungleung@gmail.com>
2024-05-02 15:40:24 -0400

    linted

--- map.js
+++ map.js
@@ -1,14 +1,42 @@
-const map = function (array, callback) {
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  if (arr1.length !== arr2.length) {
+    result = false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let equal = eqArrays(actual, expected);
+  if (equal === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  }
+};
+
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
-    console.log("item BEFORE: ", item);
-    console.log("item AFTER: ", callback(item));
-    console.log("---");
+    results.push(callback(item));
   }
   return results;
 };
 
-const words = ["ground", "control", "to", "major", "tom"];
+const words = ["great", "cool", "to", "major", "tom"];
 
 const results1 = map(words, (word) => word[0]);
 console.log(results1);
+
+const results2 = map(words, (word) => word.length);
+
+const results3 = map(words, (word) => word + "s");
+
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+assertArraysEqual(results2, [5, 4, 2, 5, 3]);
+assertArraysEqual(results3, ["greats", "cools", "tos", "majors", "toms"]);
