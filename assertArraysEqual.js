5ea2740503ab17855d254baba7154d210f7f796b
ihleung <ihfungleung@gmail.com>
2024-05-06 09:12:43 -0400

    updated

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let result = true;
-  if (arr1.length !== arr2.length) {  // check if the length of the arrays is the same
-    result = false;  // if not, return false
-  }
-  for (let i = 0; i < arr1.length; i++) { // loop through the arrays
-    if (arr1[i] !== arr2[i]) { // check if the elements of the arrays are the same
-      result = false; // if not, return false
-    }
-  }
-  return result;
-};
+const eqArrays = require('../eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   let equal = eqArrays(actual, expected); // call eqArrays function
@@ -20,7 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual; // export the function to be used in other files
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+//Test Cases to see if arrays are equal
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
