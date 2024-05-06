5fc4cb385365e663364c8b1b141d5402a603bcdb
ihleung <ihfungleung@gmail.com>
2024-05-06 09:54:27 -0400

    updated

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,18 +2,8 @@ const asset  = require('chai').assert;
 const middle = require('../middle');
 
 
-
-/*
+// TEST CODE
 //Test Cases to see if arrays are equal
-assertArraysEqual(middle([1]), []); // => should PASS
-assertArraysEqual(middle([1, 2]), []); // => should PASS
-assertArraysEqual(middle([1, 2, 3]), [2]); // => should PASS
-assertArraysEqual(middle([2, 3, 5, 6, 4]), [3]); // => should FAIL
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 10, 6, 7, 8, 9]), [5]); // => should FAIL
-*/
-
 describe("#middle", () => {
   it("returns [] for [1]", () => {
     asset.deepEqual(middle([1]), []); //compare arrays to see if they are equal
