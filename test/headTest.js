b337f077ff05757954e717dde7efcfd7142aa79c
ihleung <ihfungleung@gmail.com>
2024-05-06 09:38:04 -0400

    updated

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,14 @@
-const assertEqual = require("../assertEqual");
 const head = require("../head");
+const assert = require('chai').assert;
 // TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
 
-assertEqual(head([39, 6, 7]), 31);
-assertEqual(head(["New", "Used", "Well-worn"]), "Jeans");
-assertEqual(head([]), 13);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+});
