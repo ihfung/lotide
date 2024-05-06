1d70029109e79dcdf27d911c1351dd0a45218ac7
ihleung <ihfungleung@gmail.com>
2024-05-06 09:46:58 -0400

    updated

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,22 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]); // test with 3 elements
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); //  ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");   // ensure second element is "Labs"
+// TEST CODE
+//Test Cases to see if arrays are equal
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {  //changed from assert.strictEqual to assert.deepEqual
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); //compare arrays to see if they are equal
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns [] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); //compare arrays to see if they are equal
+  });
 
-const test = tail(["Japan"]); // test with one element
-assertEqual(test.length, 0); // should return an empty array
-const testTwo = tail([]); // test with an empty array
-assertEqual(testTwo.length, 0);   // should return an empty array
+  it("returns [] for ['Japan']", () => {
+    assert.deepEqual(tail(['Japan']), []); //compare arrays to see if they are equal
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []); //compare arrays to see if they are equal
+  });
+});
