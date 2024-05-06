a5246e01c908663903481ca6455ec96767c0d079
ihleung <ihfungleung@gmail.com>
2024-05-06 09:22:44 -0400

    updated

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,15 +3,6 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 
 //Test Cases to see if arrays are equal
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
-middle([1, 2, 3, 4, 5, 6, 7, 8, 9]); // => [5]
-middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]); // => [5, 6]
-middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]); // => [6]
 
 assertArraysEqual(middle([1]), []); // => should PASS
 assertArraysEqual(middle([1, 2]), []); // => should PASS
