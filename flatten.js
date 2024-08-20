7f36a7e19c2efe6fbca0d9fa6178e6f6b7951d9a
ihleung <ihfungleung@gmail.com>
2024-08-20 12:07:33 -0400

    added flatten from stretch activites

---
+++ flatten.js
@@ -0,0 +1,41 @@
+const assertArraysEqual = function(actual, expected) {
+  let equal = eqArrays(actual, expected); // call eqArrays function
+  if (equal === false) { // check if the return value is false
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  if (arr1.length !== arr2.length) { // check if the length of the arrays is the same
+    result = false;
+  }
+  for (let i = 0; i < arr1.length; i++) { // loop through the arrays
+    if (arr1[i] !== arr2[i]) { // check if the elements of the arrays are the same
+      result = false;
+    }
+  }
+  return result;
+};
+
+
+const flatten = function(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) { //check if the element is an array
+      for (let j = 0; j < arr[i].length; j++) {
+        newArr.push(arr[i][j]);
+      }
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+
