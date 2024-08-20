1c6b696175ce53a4849f2d90bfc513c377438c89
ihleung <ihfungleung@gmail.com>
2024-08-20 12:14:24 -0400

    updated flatten

--- flatten.js
+++ flatten.js
@@ -3,31 +3,27 @@ const assertArraysEqual = function(actual, expected) {
   if (equal === false) { // check if the return value is false
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   } else {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
 const eqArrays = function(arr1, arr2) {
-  let result = true;
   if (arr1.length !== arr2.length) { // check if the length of the arrays is the same
-    result = false;
+    return false;
   }
   for (let i = 0; i < arr1.length; i++) { // loop through the arrays
     if (arr1[i] !== arr2[i]) { // check if the elements of the arrays are the same
-      result = false;
+      return false;
     }
   }
-  return result;
+  return true;
 };
-
 
 const flatten = function(arr) {
   let newArr = [];
   for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) { //check if the element is an array
-      for (let j = 0; j < arr[i].length; j++) {
-        newArr.push(arr[i][j]);
-      }
+    if (Array.isArray(arr[i])) { 
+      newArr = newArr.concat(arr[i]); // use concat to flatten the array. concat is used to merge two or more arrays
     } else {
       newArr.push(arr[i]);
     }
@@ -35,13 +31,12 @@ const flatten = function(arr) {
   return newArr;
 };
 
+// Unique Test Cases
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => ✅ Assertion Passed
+assertArraysEqual(flatten([1, [2, 3], [4, 5], 6]), [1, 2, 3, 4, 5, 6]); // => ✅ Assertion Passed
+assertArraysEqual(flatten([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5, 6]); // => ✅ Assertion Passed
 
-
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
-assertArraysEqual(flatten([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
-//fail case
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5]); // => [1, 2, 3, 4, 5]
-assertArraysEqual(flatten([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5]); // => [1, 2, 3, 4, 5]
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5]); // => [1, 2, 3, 4, 5]
-
+// Fail Cases
+assertArraysEqual(flatten([1, 2, [3, 4], 5]), [1, 2, 3, 4, 5, 6]); // => 🛑 Assertion Failed
+assertArraysEqual(flatten([1, [2, 3], 4, 5]), [1, 2, 3, 4, 6]); // => 🛑 Assertion Failed
+assertArraysEqual(flatten([1, [2], [3, 4], 5]), [1, 2, 3, 4]); // => 🛑 Assertion Failed
